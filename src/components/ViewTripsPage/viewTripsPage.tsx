0a071eed10b3ca41363f5b076e2a6fd2323d9116
Celia Bocking <celiabocking99@gmail.com>
2023-01-09 15:57:31 +0000

    map trips

--- src/components/ViewTripsPage/viewTripsPage.tsx
+++ src/components/ViewTripsPage/viewTripsPage.tsx
@@ -1,8 +1,42 @@
 import React from 'react'
 
+const tripsArr = [
+    {
+        tripName: 'Julia hen do',
+        startDate: '2023/06/03',
+        endDate: '2023/06/06',
+        location: 'Paris'
+
+    },
+
+    {
+        tripName: 'Work christmas meal',
+        startDate: '2023/12/18',
+        endDate: '2023/12/18',
+        location: 'London'
+    },
+     {
+        tripName: 'Girls trip',
+        startDate: '2023/08/01',
+        endDate: '2023/08/12',
+        location: 'Ibiza'
+     }
+
+]
+
 const ViewTrips = () => {
-    return <>
-    </>
+    return (
+        <ul>
+            {tripsArr.map((trip) => {
+                return (<li>
+                    <p>{trip.tripName}</p>
+                    <p>{trip.startDate} {trip.endDate}</p>
+                    <p>{trip.location}</p>
+                </li>)
+            })}
+        </ul>
+    )
+
 }
 
 export default ViewTrips

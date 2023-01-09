53222523466b51d7fb23cc4b23287b12a9dc651a
Muhammed Atyib Ali <aatyib@gmail.com>
2023-01-09 15:45:51 +0000

    starting ticket for naviagation bar Co-authored-by: author-name Natasha.chinea@gmail.com

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,84 @@
+// import react from "react";
+import logo from "../../.././src/logo.svg"
+
+
+const Navbar = () => {
+	 return (
+	 	<nav>
+			<img src={logo} alt="logo" />
+			<button> Dashboard </button>
+			<button>Login</button>
+ 	
+	 	</nav>
+	 );
+	 };
+	
+	 export default Navbar;
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import { Nav, NavLink, NavMenu }
+//  	from "./NavBarElements.js"
+
+// const Navbar = () => {
+// return (
+// 	<>
+// 	<Nav>
+// 		<NavMenu>
+// 		<NavLink to="/Dashboard" activeStyle>
+// 			About
+// 		</NavLink>
+// 		<NavLink to="/contact" activeStyle>
+// 			Contact Us
+// 		</NavLink>
+// 		<NavLink to="/blogs" activeStyle>
+// 			Blogs
+// 		</NavLink>
+// 		<NavLink to="/sign-up" activeStyle>
+// 			Sign Up
+// 		</NavLink>
+// 		</NavMenu>
+// 	</Nav>
+// 	</>
+// );
+// };
+
+// export default Navbar;

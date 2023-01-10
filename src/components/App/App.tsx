b3f9e857d367ce881ae2cd227bdf80bf2db458ec
Natasha <112275600+natashachinea@users.noreply.github.com>
2023-01-10 10:46:59 +0000

    Auth 0

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,15 +3,22 @@ import './App.css';
 import Login from '../Login/login'
 import Dashboard from '../Dashboard/dashboard'
 import Navbar from '../NavBar/NavBar';
+import { Auth0Provider } from "@auth0/auth0-react";
+import LoginButton from "../LoginButton/LoginButton";
+import LogoutButton from "../LogoutButton/LogoutButton";
 
 function App() {
-  return (<>
-
-
-    <Navbar></Navbar>
-    <Login></Login>
-    <Dashboard></Dashboard>
+  return (
+      <>
+       <Auth0Provider domain= "dev-l6bnebnlo0sm3iul.uk.auth0.com" scope="read:current_user update:current_user_metadata" clientId="iu5ZM51QPbpIVQdwYw43MqvSk2htHc51" >
+           <LoginButton/>
+           <LogoutButton/>
+        <Navbar></Navbar>
+        <Login></Login>
+        <Dashboard></Dashboard>
+      </Auth0Provider>
     </>
+
   );
 }
 

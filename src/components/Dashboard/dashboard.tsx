e9c0a1f58035096b17e7c51389f548e188d63054
Kylie Smaldon <k_smaldon@outlook.com>
2023-01-10 14:26:10 +0000

    added initial styling
    co-authored-by: Dionne dionne.soperdyer@gmail.com

--- src/components/Dashboard/dashboard.tsx
+++ src/components/Dashboard/dashboard.tsx
@@ -1,54 +1,79 @@
-import React, {useState} from 'react'
-import CreateTrip from '../CreateTrip/createTrip'
-import JoinTrip from '../JoinTripPage/joinTrip'
-import ViewTrips from '../ViewTripsPage/viewTripsPage'
-import TripDetails from '../TripDetailsPage/tripDetailsPage'
+import React, { useState } from "react";
+import './dashboard.css'
+import CreateTrip from "../CreateTrip/createTrip";
+import JoinTrip from "../JoinTripPage/joinTrip";
+import ViewTrips from "../ViewTripsPage/viewTripsPage";
+import TripDetails from "../TripDetailsPage/tripDetailsPage";
 
+import { AiOutlineUsergroupAdd } from "react-icons/ai";
+import { IoMdCreate } from "react-icons/io";
+import { ImBinoculars } from "react-icons/im";
 
 const Dashboard = () => {
+  const [createTripVisibility, setCreateTripVisibility] = useState(false);
+  const [joinTripVisibility, setJoinTripVisibility] = useState(false);
+  const [viewTripVisibility, setViewTripVisibility] = useState(false);
 
-    const [createTripVisibility, setCreateTripVisibility] = useState(false)
-    const [joinTripVisibility, setJoinTripVisibility] = useState(false)
-    const [viewTripVisibility, setViewTripVisibility] = useState(false)
+  function handleCreateTripVisibility() {
+    setCreateTripVisibility((current) => !current);
+  }
 
-    function handleCreateTripVisibility() {
-        setCreateTripVisibility((current) => !current)
-    }
+  function handleJoinTripVisibility() {
+    setJoinTripVisibility((current) => !current);
+  }
 
-    function handleJoinTripVisibility() {
-        setJoinTripVisibility((current) => !current)
-    }
+  function handleViewTripVisibility() {
+    setViewTripVisibility((current) => !current);
+  }
 
-    function handleViewTripVisibility() {
-        setViewTripVisibility((current) => !current)
-    }
+  return (
+    <section id="dashboard">
+      <h1>Where are we going?</h1>
+      <div id="dashboard-btn-container">
+        <div
+          onClick={() => {
+            handleCreateTripVisibility();
+          }}
+        >
+          <IoMdCreate />
+          <h3>create trip</h3>
+        </div>
 
-    return (<>
+        <div style={{ display: createTripVisibility ? "block" : "none" }}>
+          <CreateTrip></CreateTrip>
+        </div>
 
-    <h1>Where are we going?</h1>
+        <div
+          onClick={() => {
+            handleJoinTripVisibility();
+          }}
+        >
+          <AiOutlineUsergroupAdd />
+          <h3>join trip</h3>
+        </div>
 
-    <button onClick={() => {handleCreateTripVisibility()}}>Create Trip</button>
+        <div style={{ display: joinTripVisibility ? "block" : "none" }}>
+          <JoinTrip visibility={handleJoinTripVisibility}></JoinTrip>
+        </div>
 
-    <div style={{visibility: createTripVisibility ? 'visible' : 'hidden'}}>
-        <CreateTrip></CreateTrip>
-    </div>
+        <div
+          onClick={() => {
+            handleViewTripVisibility();
+          }}
+        >
+         <ImBinoculars/>
+          <h3>view trip</h3>
+        </div>
 
-    <button onClick={() => {handleJoinTripVisibility()}}>Join Trip</button>
-
-    <div style={{visibility: joinTripVisibility ? 'visible' : 'hidden'}}>
-        <JoinTrip visibility={handleJoinTripVisibility}></JoinTrip>
-    </div>
-
-    <button onClick={() => {handleViewTripVisibility()}}>View Trip</button>
-
-    <div style={{visibility: viewTripVisibility ? 'visible' : 'hidden'}}>
-        <ViewTrips></ViewTrips>
-    </div>
-
-    <div>
+        <div style={{ display: viewTripVisibility ? "block" : "none" }}>
+          <ViewTrips></ViewTrips>
+        </div>
+      </div>
+      <div>
         <TripDetails></TripDetails>
-    </div>
-    </>
-)}
+      </div>
+    </section>
+  );
+};
 
-export default Dashboard
+export default Dashboard;

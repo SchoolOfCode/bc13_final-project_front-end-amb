4f7df859584f6ffc540f361b8e54c8c6a32d1e3e
Celia Bocking <celiabocking99@gmail.com>
2023-01-17 14:01:34 +0000

    slogan

--- src/components/Login/login.tsx
+++ src/components/Login/login.tsx
@@ -16,6 +16,14 @@ const Login = ({pageSelect}:any) => {
        {/* <div className="flex-item">
         <LoginButton/>
         </div> */}
+<div className='sloganBox'>
+        
+        <h1 className='slogan'>
+
+Feeling decision-fatigued? <b></b> Let everyone else decide, with a vote. <br></br> <li></li>
+
+Don’t want to be the one to bring up money? <b></b> State your budget, anonymously. <br></br> <li></li> Getting ignored in the group chat? <b></b> Have your preferences heard, with Travel Herd. </h1>
+ </div>
 
         <button className="getStarted" onClick={() => {pageSelect("dashboard")}}>get started</button>
 

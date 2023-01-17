c64638f3e015c9eb4ebca62fc06cf1f0d3332f1c
Celia Bocking <celiabocking99@gmail.com>
2023-01-17 16:18:25 +0000

    tinkering

--- src/components/CreateTrip/createTrip.test.tsx
+++ src/components/CreateTrip/createTrip.test.tsx
@@ -1,1 +1,13 @@
-export {}
+
+import { test } from "@jest/globals";
+import { screen, render } from '@testing-library/react';
+import CreateTrip from "./createTrip";
+
+test("is create trip h1 rendering", () =>{
+    render(<CreateTrip />)
+    
+    const h1 = screen.getByText("Create Trip");
+    expect(h1).toBeInTheDocument();
+
+    // screen.debug();
+});
